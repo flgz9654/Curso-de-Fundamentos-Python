514dab8e550ae09beb99c047c8bbdd585ed3d4b2
Francisco Garcia <fran9415@hotmail.com>
2022-10-20 18:01:28 -0500

    Carga de los primeros archivos del curso fundamentos de Python

---
+++ 0.PrimerosPasos.py
@@ -0,0 +1,23 @@
+print("Hola. esto es Python")
+
+print("Hola Soy Francisco y tengo 28 años")
+
+# Operaciones Matematicas:
+
+print(12 + 5)
+print(10 - 5)
+print(12 * 5)
+print(12 / 5)
+
+# Esto es un comentario
+"""
+Este es un
+comentario
+de varias
+lineas
+"""
+'''
+Este tambien
+es un comentario
+de varias lineas
+'''
---
+++ 1.ClaseVariables.py
@@ -0,0 +1,15 @@
+# Esto es una variable con un string
+my_name = 'Francisco'
+print(my_name)
+
+# Esto es una variable con un numero
+my_age = 12
+print(my_age)
+
+my_name = 'Luis'
+print('Aqui cambio', my_name)
+
+# input instruccion para pedir por pantalla informacion
+
+my_name = input("Cuales tu nombre? ")
+print("Usando input:", my_name)
