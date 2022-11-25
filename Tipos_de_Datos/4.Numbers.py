ddbea04cf63468ebba547f1a252a33ebb39bd5d9
Francisco Garcia <fran9415@hotmail.com>
2022-11-24 21:04:47 -0500

    agregando clases de Numbers y Booleans, organizando todas las clases en carpeta Tipo_de_datos

---
+++ Tipos_de_Datos/4.Numbers.py
@@ -0,0 +1,46 @@
+# Los numeros no se declaran entre comillas
+
+lives = 3
+print(type(lives))
+
+age = 28
+budget_usd = 100
+temperature = 28.7
+print(type(temperature))
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+# forma abreviada para restar un numero 
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+# forma abreviada para restar un numero 
+
+lives += 5
+print(lives)
+
+number = 45000000000000000000000000000000000.1
+print(number)
+
+number_b = 0.000000000000000000000000000001
+print(number_b)
+
+ene = 1200
+feb = 1500
+mar = 4500
+
+mean = (ene + feb + mar) / 3
+print(mean)
