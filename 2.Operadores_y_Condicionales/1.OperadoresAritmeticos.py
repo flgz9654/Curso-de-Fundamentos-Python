fbc21558fd5b5d0a908fb34e54766a9fde09263f
Francisco Garcia <fran9415@hotmail.com>
2022-11-28 14:53:26 -0500

    Agregando modulo de operadores y condicionales y la 1era clase

---
+++ 2.Operadores_y_Condicionales/1.OperadoresAritmeticos.py
@@ -0,0 +1,18 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2) # 5
+print(10 % 2) # Division exacta el residuo es 0
+print(10 / 3) 
+print(10 % 3) # Division inexacta el residuo es 1 para determinar si un numero es par o impar
+print(10 // 3) # Este operador ignora los decimales y deja el resultado en entero 
+print(2 ** 3) # Operador de exponente
+print(2 ** 3 + 3 - 7 / 1 // 4) # Python sigue el orden de las operaciones aritmeticas
+print(2 ** 3)
+print((7 / 1 ) // 4)
+print(8 + 3 - 1)
+
+print("Hola" + " mundo")
+print("Hola " * 3)
+
+
