789a626d9901756695ca98431eac6e67ccdf1b70
Francisco Garcia <fran9415@hotmail.com>
2022-11-29 17:44:45 -0500

    Agregando nuevas clases para el modulo de operadores y condicionales

---
+++ 2.Operadores_y_Condicionales/4.Comparadores.py
@@ -0,0 +1,39 @@
+# Comparadores con numeros
+# > Mayor que
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+
+# < Menor que
+print(5 < 6)
+print(6 < 5)
+print(5 < 5)
+
+# >= Mayor o Igual
+print(2 >= 1)
+print(2 >= 3)
+print(2 >= 2)
+
+# <= Menor o Igual
+print(1 <= 2)
+print(2 <= 1)
+print(2 <= 2)
+
+# == Igualdad
+print(6 == 6)
+print(5 == 2)
+
+# != Diferencia
+print(6 != 10)
+print(6 != 6)
+
+
+# Comparadores con String
+
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1)
+
+age = 15
+print(age >= 18)
