d6f36feecc3ba90551a4ddfffb56a80186dff602
Francisco Garcia <fran_9415@LAPTOP-5740OPKK>
2023-01-16 20:24:42 -0500

    Agregando clases de listas y metodos

---
+++ 3.Listas_Diccionarios/3.Listas.py
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(type(numbers))
+
+task = ['make a dishes', 'play videogames']
+print(task)
+
+types = [1, True, 'Hola']
+print(types)
+
+print(numbers[0])
+print(task[0])
+text = 'Hola'
+# text[0] = 'W'
+
+task[0] = 'watch platzi courses'
+print(task)
+
+task[0] = 'do the dishes'
+print(task)
+
+print(numbers[:3])
+print(True in types)
+print('Hola' in types)
---
+++ 3.Listas_Diccionarios/4.Metodos_Listas.py
@@ -0,0 +1,16 @@
+# CRUD Create, Read, Update & Delete
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[1])
+numbers[-1] = 10
+
+print(numbers)
+
+numbers.append(700)
+print(numbers)
+
+numbers.insert(0, 'Hola')
+print(numbers)
+
+numbers.insert(3, 'change')
+print(numbers)
